032a4b6c39fa8a948d06c7c5af6cb9096cbab18c
John Sorensen <john.sorensen@usda.gov>
2025-02-04 10:32:50 -0600

    Module functions successfully

---
+++ src/addSeasons.py
@@ -0,0 +1,25 @@
+import sqlite3
+import pandas as pd
+
+class seasons():
+
+    def __init__(self, database):
+        self.database = database
+        self.conn = sqlite3.connect(self.database)
+        self.cur = self.conn.cursor()
+
+    def addSeasons(self, filename):
+        df = pd.read_excel(filename, sheet_name = "Data Sheet")
+        year_season = df['Year/Season Mix'].unique().tolist()
+        for entry in year_season:
+            year = entry.split(' ')[0]
+            season = entry.split(' ')[1]
+            self.cur.execute('''INSERT INTO Season (
+                        Year,
+                        Season)
+                        VALUES (?, ?)''', (
+                        year,
+                        season,)
+                       )
+            self.conn.commit()
+        
---
+++ src/addTreatments.py
@@ -0,0 +1,29 @@
+import sqlite3
+import pandas as pd
+
+class treatments:
+
+    def __init__(self, database):
+        self.database = database
+        self.conn = sqlite3.connect(database)
+        self.cur = self.conn.cursor()
+
+    def addTreatments(self, filename):
+        treatment_df = pd.read_csv(filename)
+        for record in treatment_df.index:
+            plot_id = int(treatment_df.iloc[record]['Plots_id'])
+            seeding = treatment_df.iloc[record]['Seeding']
+            fert = treatment_df.iloc[record]['Fert. Treatment']
+            till = treatment_df.iloc[record]['Till. Treatment']
+            self.cur.execute('''INSERT INTO Treatments (
+                        Plots_id,
+                        Seeding,
+                        Fertilizer_Treatment,
+                        Tillage_Treatment)
+                        VALUES (?, ?, ?, ?)''', (
+                        plot_id,
+                        seeding,
+                        fert,
+                        till,)
+                       )
+            self.conn.commit()
